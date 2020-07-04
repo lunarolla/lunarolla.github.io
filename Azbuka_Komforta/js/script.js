77bb1e0bac8c07b59aca2c746753fb28af7d0753
lunarolla <42235066+lunarolla@users.noreply.github.com>
2020-07-04 22:40:27 +0300

    verstra_finish

--- Azbuka_Komforta/js/script.js
+++ Azbuka_Komforta/js/script.js
@@ -1,36 +1,10 @@
-var modal = document.getElementById("myModal");
-        var btn = document.getElementById("myBtn");
-        var btn1 = document.getElementById("myBtn1");
-        var span = document.getElementsByClassName("close")[0];
-        btn.onclick = function() {
-          modal.style.display = "block";
-        }
-        btn1.onclick = function() {
-          modal.style.display = "block";
-        }
-        span.onclick = function() {
-          modal.style.display = "none";
-        }
-        window.onclick = function(event) {
-          if (event.target == modal) {
-            modal.style.display = "none";
-          }
-        }
-
-        window.addEventListener('DOMContentLoaded', () => {
-            const menu = document.querySelector('.header_menu'),
-            menuItem = document.querySelectorAll('.header_menu_item'),
-            hamburger = document.querySelector('.hamburger');
-
-            hamburger.addEventListener('click', () => {
-                hamburger.classList.toggle('hamburger_active');
-                menu.classList.toggle('header_menu_active');
-            });
-
-            menuItem.forEach(item => {
-                item.addEventListener('click', () => {
-                    hamburger.classList.toggle('hamburger_active');
-                    menu.classList.toggle('header_menu_active');
-                })
-            })
-        })
+$('#inline-popups').magnificPopup({
+  delegate: 'a',
+  removalDelay: 500, //delay removal by X to allow out-animation
+  callbacks: {
+    beforeOpen: function() {
+       this.st.mainClass = this.st.el.attr('data-effect');
+    }
+  },
+  midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+});
