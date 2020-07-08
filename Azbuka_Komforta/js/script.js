8665e60d44318f07adc4c9a6aed7f1d9427271d9
lunarolla <42235066+lunarolla@users.noreply.github.com>
2020-07-08 21:12:47 +0300

    new_page

--- Azbuka_Komforta/js/script.js
+++ Azbuka_Komforta/js/script.js
@@ -12,3 +12,19 @@ $('#inline-popups, #inline-popups2').magnificPopup({
   $(".hamburger").click(function() {
   $(this).toggleClass('close');
   });
+
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
