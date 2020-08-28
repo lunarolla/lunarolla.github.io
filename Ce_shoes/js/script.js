7d7e9c274ba7974fc7f3aa8314bc85e8213d8b76
lunarolla <42235066+lunarolla@users.noreply.github.com>
2020-08-28 23:46:26 +0300

    new_cite

---
+++ Ce_shoes/js/script.js
@@ -0,0 +1,8 @@
+$('.slick-carousel').slick({
+    infinite: true,
+    slidesToShow: 1, 
+    slidesToScroll: 1,
+    dots: true,
+    prevArrow: "<img src='icons/left.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='icons/right.svg' class='next' alt='2'>",
+  });
