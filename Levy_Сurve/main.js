452e5545845cab58338de21c5413500d5399dd78
lunarolla <42235066+lunarolla@users.noreply.github.com>
2020-10-21 10:57:26 +0300

    lastchange

--- Levy_Сurve/main.js
+++ Levy_Сurve/main.js
@@ -1,6 +1,7 @@
 $( document ).ready( function() {
 
     var $canvas         = $( 'canvas#my_canvas' );
+    //создержимое моего поля для прорисовки фрактала
     var ctx             = $canvas[0].getContext('2d');
     var canvas_width    = 820; // ширина поял для рисунка
     var canvas_height   = 540; // высота поял для рисунка
@@ -12,46 +13,45 @@ $( document ).ready( function() {
     var line_width      = 2;
 
     var default_angle   = 45;
-
+    // цвета котрые используются во фрактале
     var colors = [ '#fc0398', '#00CC00', '#FFFF00', '#123EAB' ];
-    
+    //переводит градусы в радианы
     var toRadians = function( d ) {
     return Math.PI*(d/180.0);
     };
-    
-    var paint = function(ctx) {
-        c_curve(x, y, len, alpha_angle, iteration_n, ctx);
-    };
-
+   
+    //получение рандомного числа со сбросом остатка
     var mrand_i = function(max) {
     return Math.floor( Math.random()*max );
     };
-
-    var draw = function() {
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0,0,820,540);
-
-    iteration_count = getIntegerInput('iterations');
-    line_width      = getIntegerInput('line_width');
-    anim_speed      = getIntegerInput('anim_speed');
-
-    var i = 1;                    
-
-    function myLoop () {           
-       setTimeout(function () {  
-        ctx.fillRect(0,0,820,540); 
- 
-          c_curve( x, y, len, alpha_angle, i, ctx );      
-          i++;                     
-          if (i < iteration_count) {            
-             myLoop();            
-          }                       
-       }, anim_speed)
-    }
-
-    myLoop();                  
-    };
     
+    var draw = function() {
+        //рисует поле для фрактала
+        ctx.fillStyle = 'black';
+        ctx.fillRect(0,0,820,540);
+        //мои переданные значения
+        iteration_count = getIntegerInput('iterations');
+        line_width      = getIntegerInput('line_width');
+        anim_speed      = getIntegerInput('anim_speed');
+
+        var i = 1;                    
+        //рекурсия
+        function myLoop () {          
+            //зодержка по времени передаю функцию с отрисовкой фрактала и само время (anim_speed)
+            setTimeout(function () {  
+                ctx.fillRect(0,0,820,540); 
+                //отрисовка пошагово фрактала
+                c_curve( x, y, len, alpha_angle, i, ctx );      
+                i++;                     
+                if (i < iteration_count) {            
+                    myLoop();            
+                }                       
+            }, anim_speed)
+        }
+        //вызов моей функции
+        myLoop();                  
+    };
+    //отрисовка фрактала
     var c_curve = function( x, y, length, angle, iteration, ctx  ) {
         var alpha  = angle;
         if( iteration > 0 ) {
@@ -64,25 +64,25 @@ $( document ).ready( function() {
             ctx.strokeStyle = colors[ mrand_i(colors.length+1) ]; 
         ctx.lineWidth   = line_width;
         ctx.lineCap = 'round';
+        //установка в начало
         ctx.beginPath();
+        //продвижение
         ctx.moveTo( x, y );
+        //продвижение
         ctx.lineTo( x + (length * Math.cos(toRadians(alpha))),
             y + (length * Math.sin(toRadians(alpha))) );
+        //прорисовка
         ctx.stroke();
         }
     };
     draw();  
-
+    //события ДОМ (прорисовка по изменению поля ввода)
     $( 'input#iterations' ).change( draw );
     $( 'input#line_width' ).change( draw );
     $( 'input#anim_speed' ).change( draw );
    
 } );
-
-var getFloatInput = function(id) {
-    return parseFloat( document.getElementById(id).value );
-}
-
+//в самом начале заполняю переменны
 var getIntegerInput = function(id) {
     return parseInt( document.getElementById(id).value );
 }
